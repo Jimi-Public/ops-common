51b8c1ecf6fbc4f24aafc38d1c4f3e78a3128cc6
王超群 <wangchaoqun@jimilab.com>
2023-02-09 20:49:13 +0800

    feat(init): init
    init

---
+++ jwt/jwt.go
@@ -0,0 +1,82 @@
+/*
+@Project: ops-common
+@Author:  WangChaoQun
+@Date:    2023/2/9
+@IDE:     GoLand
+@File:    jwt.go
+*/
+
+package jwt
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type TokenInterface interface {
+	GenerateToken(username string) (string, error)
+	ParseToken(token string) (*Claims, error)
+}
+
+type Token struct {
+	JwtSecret  string // 加密秘钥
+	ExpireTime int    // 多少小时过期
+}
+
+func NewToken(f ...func(token *Token)) TokenInterface {
+	t := &Token{}
+	for _, i := range f {
+		i(t)
+	}
+	return t
+}
+
+func OptionWithJwtSecret(s string) func(token *Token) {
+	return func(token *Token) {
+		token.JwtSecret = s
+	}
+}
+
+func OptionWithExpireTime(t int) func(token *Token) {
+	return func(token *Token) {
+		token.ExpireTime = t
+	}
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func (t *Token) GenerateToken(username string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(time.Duration(t.ExpireTime) * time.Hour)
+
+	claims := Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "jimi-ops",
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(t.JwtSecret)
+
+	return token, err
+}
+
+func (t *Token) ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return t.JwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
