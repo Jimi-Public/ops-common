0848bbdbdd956248cbd81951d9da13408e6d091e
王超群 <wangchaoqun@jimilab.com>
2023-04-24 15:08:58 +0800

    更新response/responseCode.go, jwt/middleware.go

--- response/responseCode.go
+++ response/responseCode.go
@@ -12,6 +12,8 @@ package response
 const (
 	SuccessCode   Code = 20000 // 成功状态码
 	ForbiddenCode Code = 21000 // 操作拒绝
+	ParamsErr     Code = 23000 // 参数异常
 	FailCode      Code = 22000 // 操作失败 后端异常
 	AuthFail      Code = 40300 // 认证失败
+	TokenExpire   Code = 40301 // token 过期 
 )
